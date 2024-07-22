39250a930c9fdc0cb7c45dc5019a98ece8fae277
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-07-22 11:15:36 +0200

    add faster r-cnn model

--- mainprocess/run_fasterrcnn_training.py
+++ mainprocess/run_fasterrcnn_training.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import detectron2
 
@@ -8,7 +9,18 @@ from detectron2.data import MetadataCatalog, DatasetCatalog
 
 from detectron2.utils.logger import setup_logger
 
+import yaml
+
+# Function to load YAML file
+def load_yaml():
+    yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_dataset.fasterrcnn.yaml'))
+    print(yaml_path)
+    
+
 def regist_custom_dataset():
     regist_coco_instances(
-
+        'my_dataset_train', # Name of my custom dataset
+        {}, # metadata
+        annotations_path, # path to annotations
+        images_path # path to training images
     )
