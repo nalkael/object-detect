4d2c71caf66f0217e993dfbde3f80ae62b29c009
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-07-22 15:47:20 +0200

    set custom dataset

--- mainprocess/run_fasterrcnn_training.py
+++ mainprocess/run_fasterrcnn_training.py
@@ -6,21 +6,59 @@ import detectron2
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog, DatasetCatalog
-
+from detectron2.data.datasets import register_coco_instances
 from detectron2.utils.logger import setup_logger
 
 import yaml
+import cv2
 
 # Function to load YAML file
-def load_yaml():
-    yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_dataset.fasterrcnn.yaml'))
-    print(yaml_path)
-    
+def load_yaml(yaml_path):
+    # yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_dataset.fasterrcnn.yaml'))
+    with open(yaml_path, 'r') as file:
+        config = yaml.safe_load(file)
+        return config
 
-def regist_custom_dataset():
-    regist_coco_instances(
+
+def register_custom_dataset(yaml_path):
+    # yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_dataset.fasterrcnn.yaml'))
+    config = load_yaml(yaml_path)
+    train_images_path = config['train']
+    train_annotations_path = config['train_annotation']
+    val_images_path = config['val']
+    val_annotations_path = config['val_annotation']
+
+    # Get current absolute path for images path and annotation path of training dataset
+    train_images_abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), train_images_path))
+    train_annotation_abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), train_annotations_path))
+
+
+    val_images_abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), val_images_path))
+    val_annotation_abspath = os.path.abspath(os.path.join(os.path.dirname(__file__), val_annotations_path))
+
+    '''
+    print(train_annotation_abspath)
+    print(train_images_abspath)
+    print(val_annotation_abspath)
+    print(val_images_abspath)
+
+    '''
+
+    register_coco_instances(
         'my_dataset_train', # Name of my custom dataset
         {}, # metadata
-        annotations_path, # path to annotations
-        images_path # path to training images
+        train_annotation_abspath, # path to annotations
+        train_images_abspath # path to training images
     )
+
+    register_coco_instances(){
+        'my_dataset_val',
+        {},
+        val_annotation_abspath,
+        val_images_abspath
+    }
+    
+
+yaml_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'my_dataset.fasterrcnn.yaml'))
+
+register_custom_dataset(yaml_path)
