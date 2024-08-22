b4c4cc9b840952c2c907e881159596a995b55931
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-08-22 16:53:06 +0200

    add dataset version

--- mainprocess/ssd-detectron2/foo.py
+++ mainprocess/ssd-detectron2/foo.py
@@ -7,7 +7,7 @@ import detectron2
 
 # Setup detectron2 logger
 from detectron2.utils.logger import setup_logger
-setuo_logger()
+setu_logger()
 # Debugging, Monitoring, Standardized Logging, Error Reporting
 # setup_logger() might not be strictly necessary
 # but important in complex or production environments
