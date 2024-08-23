1855c5752828b14ede2bb82a3af618cbfa1fdfb6
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-08-23 17:06:29 +0200

    changed file structure

--- mainprocess/ssd/build_model.py
+++ mainprocess/ssd/build_model.py
@@ -4,3 +4,14 @@ It loads a base model, applies custom configurations, and sets up training param
 You might include a function that returns a fully configured cfg object.
 '''
 
+import torch
+from torchvision.models.detection import ssd300_vgg16
+
+def get_model(num_class):
+    """
+    Load and modify the SSD model for the given number of classes
+    """
+
+    # Load an SSD model pre-trained on COCO
+    model = ssd300_vgg16(pretrained=True)
+    pass
