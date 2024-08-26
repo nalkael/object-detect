f202ee80c6374f0556af06df76986302ab5ecb40
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-08-26 15:42:08 +0200

    catastophe

--- mainprocess/ssd/build_model.py
+++ mainprocess/ssd/build_model.py
@@ -8,19 +8,33 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
+from torchvision.models.detection.ssd import SSDClassificationHead
+from torchvision.models.detection import _utils
 from torchvision.models.detection import ssd300_vgg16
+from torchvision.models.detection import SSD300_VGG16_Weights
 
-def get_model(num_class):
+def get_model(num_class=91, size=300):
     """
     Load and modify the SSD model for the given number of classes
     """
 
-    # Load an SSD model pre-trained on COCO
-    model = ssd300_vgg16(pretrained=True)
+    # Load an SSD model pre-trained on COCO (Load the Torchvision pretrained model)
+    model = ssd300_vgg16(weights=SSD300_VGG16_Weights.DEFAULT)
 
     # Modify the classification head to match the number of classes
-    in_channels = model.head.classification_head[0].in_channels
-    pass
+    '''
+    This function calculates the number of output channels from the backbone model (VGG16 in this case) w
+    hen given an input image of a specific size (in this case, size x size). 
+    This number of channels is needed to properly configure the SSD classification head.
+    '''
+    in_channels = _utils.retrieve_out_channels(model.backbone, (size, size))
 
-model = models.vgg16(pretrained=True)
-print(model.classifier)
+    # List containing number of anchors
+    num_anchors = 1
+
+
+    model.head.classification_head = SSDClassificationHead(
+        in_channels=in_channels,
+        num_anchors=num_anchors,
+        num_classes=num_classes,
+    )
