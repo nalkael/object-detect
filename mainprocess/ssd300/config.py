4106a373c757c5fabfc41db3fb9c3acb91c5ac99
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-10-11 16:16:34 +0200

    add ssd300

--- mainprocess/ssd300/config.py
+++ mainprocess/ssd300/config.py
@@ -10,4 +10,20 @@ NUM_WORKERS = 4 # Number of parallel workers for data loading
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # training folder
-TRAIN_DIR
+TRAIN_DIR = 'data/License Plate Recognition.v1-raw-images.voc/train'
+# Validation images and XML files directory.
+VALID_DIR = 'data/License Plate Recognition.v1-raw-images.voc/valid'
+
+# Classes: 0 index is reserved for background.
+CLASSES = [
+    '__background__', 
+    'License_Plate'
+    ]
+
+NUM_CLASSES = len(CLASSES)
+
+# Whether to visualize images after crearing the data loaders.
+VISUALIZE_TRANSFORMED_IMAGES = False
+
+# Location to save model and plots.
+OUT_DIR = 'outputs'
