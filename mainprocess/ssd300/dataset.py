f8457ec9aa8937706dc0e2e46a410b320f539276
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-10-25 15:48:50 +0200

    add some functions

--- mainprocess/ssd300/dataset.py
+++ mainprocess/ssd300/dataset.py
@@ -3,9 +3,18 @@
 # this script are only used to train and evaluate the annoated dataset
 
 import torch
+import torchvision
+from torch.utils.data import DataLoader
 import cv2
 import os
 import numpy as np
+
+from config import (
+    CLASSES, RESIZE_TO, TRAIN_DIR, BATCH_SIZE
+)
+
+from torchvision.datasets import CocoDetection
+from custom_utils import collate_fn, get_train_transform, get_valid_transform
 
 """
 # the dataset preparation also handels the images without bounding boxes
@@ -25,7 +34,16 @@ RandomGamma
 use Albumentations library to apply the augmentations
 """
 
-from config import (
-    CLASSES, RESIZE_TO, TRAIN_DIR, BATCH_SIZE
-)
+class CustomCocoDataset(CocoDetection):
+    def __init__(self, img_dir, annotation_file, width, height, transforms=None):
+        super(CustomCocoDataset, self).__init__(img_dir, annotation_file)
+        self.transforms = transforms
+        self.width = width
+        self.height = height
+
+    def __getitem__(self, index):
+        # Get image and annotation from the parent class
+        image, annotation = super(CustomCocoDataset, self).__getitem__(index)
+
+        # Resize and normalize the image
 
