bc8b082d14323651dbc83a6a5d40d27eda7ad467
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-11-12 10:21:03 +0100

    need modify config

--- mainprocess/fasterrcnn/setup.py
+++ mainprocess/fasterrcnn/setup.py
@@ -22,30 +22,44 @@ from detectron2.utils.visualizer import Visualizer
 
 from detectron2.data.datasets import register_coco_instances
 
+'''
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
+'''
 
 register_coco_instances("my_dataset_train", {},
-                        "/Users/hoshikun/Documents/object-detect-dev/datasets/chess_coco/train/_annotations.coco.json",
-                        "/Users/hoshikun/Documents/object-detect-dev/datasets/chess_coco/train")
+                        "/home/rdluhu/Dokumente/object_detection_project/datasets/dataset_coco/train/_annotations.coco.json",
+                        "/home/rdluhu/Dokumente/object_detection_project/datasets/dataset_coco/train")
 register_coco_instances("my_dataset_test", {},
-                        "/Users/hoshikun/Documents/object-detect-dev/datasets/chess_coco/test/_annotations.coco.json",
-                        "/Users/hoshikun/Documents/object-detect-dev/datasets/chess_coco/test")
+                        "/home/rdluhu/Dokumente/object_detection_project/datasets/dataset_coco/valid/_annotations.coco.json",
+                        "/home/rdluhu/Dokumente/object_detection_project/datasets/dataset_coco/valid")
 
 # visualize training data
-my_dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
+dataset_train_metadata = MetadataCatalog.get("my_dataset_train")
 dataset_train_dicts = DatasetCatalog.get("my_dataset_train")
 
-my_dataset_test_metadata = MetadataCatalog.get("my_dataset_test")
+dataset_test_metadata = MetadataCatalog.get("my_dataset_test")
 dataset_test_dicts = DatasetCatalog.get("my_dataset_test")
 
 
-for d in random.sample(my_dataset_train_dicts, 10):
+for d in random.sample(dataset_train_dicts, 10):
     img = cv2.imread(d["file_name"])
-    visualizer = Visualizer(img[:, :, ::-1], metadata=my_dataset_train_metadata)
+    visualizer = Visualizer(img[:, :, ::-1], metadata=dataset_train_metadata)
     v = visualizer.draw_dataset_dict(d)
     cv2.imshow("image", v.get_image()[:, :, ::-1])
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+for t in random.sample(dataset_test_dicts, 10):
+    img_t = cv2.imread(t['file_name'])
+    visualizer = Visualizer(img_t[:, :, ::-1], metadata=dataset_test_metadata)
+    v = visualizer.draw_dataset_dict(t)
+    cv2.imshow("image", v.get_image()[:, :, ::-1])
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    pass
 
 
