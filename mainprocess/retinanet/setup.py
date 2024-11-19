d283eb7836e8aa86051bbd71188a5edc86f54e07
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-11-19 13:37:47 +0100

    add foo samples

--- mainprocess/retinanet/setup.py
+++ mainprocess/retinanet/setup.py
@@ -9,6 +9,7 @@ print('Setup logger...')
 import numpy as np
 import os, json, cv2, random
 from cv2 import imshow
+import imghdr
 
 # import common detectron2 utilities
 from detectron2 import model_zoo
@@ -23,22 +24,29 @@ def show_info():
     print("torch: ", TORCH_VERSION, "; cuda: ", CUDA_VERSION)
     print("detectron2: ", detectron2.__version__)
 
-import imghdr
 # test a pre-trained detectron2 model
 def show_samples(file_dir):
     images = []
 
     for file_name in os.listdir(file_dir):
-        pass
-
-    im = cv2.imread('/home/rdluhu/Dokumente/object_detection_project/samples/003.jpg')
-    cv2.imshow('Image', im)
-    cv2.waitKey(0)
-
-    im = cv2.imread('/home/rdluhu/Dokumente/object_detection_project/samples/17.jpg')
-    cv2.imshow('Image', im)
-    cv2.waitKey(0)
+        file_path = os.path.join(file_dir, file_name)
+        # check if the file is an image
+        if os.path.isfile(file_path) and imghdr.what(file_path) is not None:
+            # load the image
+            image = cv2.imread(file_path)
+            if image is not None:
+                images.append(image) # add the image cv2 object to the list
+                # Display the image
+                cv2.imshow(f"Image: {file_name}", image)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+            else:
+                print(f"Warning: failed to load {file_name}")
+        else:
+            print(f"Skipping non-image file {file_name}")
+    
+    return images # return the list of image objects
 
 if __name__ == '__main__':
     show_info()
-    show_samples()
+    show_samples("/home/rdluhu/Dokumente/object_detection_project/samples")
