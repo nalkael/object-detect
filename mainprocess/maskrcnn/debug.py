091ff18c5a22d2227d370df9b9acdb751508ab54
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-11-26 16:00:06 +0100

    add some baselines

--- mainprocess/maskrcnn/debug.py
+++ mainprocess/maskrcnn/debug.py
@@ -1,6 +1,19 @@
 from setup import *
 
-show_info()
-
 import torch, detectron2
+import os, sys, json, cv2, random
+import numpy as np
+
 from detectron2.utils.logger import setup_logger
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+
+show_info()
+setup_logger()
+
+samples_path = ''
+
+cfg = get_cfg()
