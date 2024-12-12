e4c7c9d04f972a8a2fb3be842bbde1bf0d5c3830
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2024-12-12 16:16:53 +0100

    add some files

--- preprocess/read_tiff.py
+++ preprocess/read_tiff.py
@@ -5,7 +5,8 @@ import sahi
 
 def read_and_slice_tiff(tiff_path, output_dir, tile_size=512, overlap_ration=0.0):
     """
-    Read a large TIFF / GeoTIFF file and divide it into slices and save them as separate files
+    Read a large TIFF / GeoTIFF file and divide it into slices with overlap 
+    and save them as separate files
     
     Args:
         tiff_path (str): Path to the TIFF file.
@@ -16,3 +17,9 @@ def read_and_slice_tiff(tiff_path, output_dir, tile_size=512, overlap_ration=0.0
 
     with rasterio.open(tiff_path) as src:
         width, height = src.width, src.height
+        # Calculate the tile step size (width and height) for overlap
+        step_size = int(tile_size * (1 - overlap_ration))
+
+        tile = []
+        for y in range(0, height, step_size):
+            pass
