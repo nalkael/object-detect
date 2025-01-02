2004bf6e24728d54212e76afa74c3755b65f063f
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2025-01-02 14:18:20 +0100

    build pipeline

--- preprocess/read_tiff.py
+++ preprocess/read_tiff.py
@@ -1,9 +1,10 @@
 import rasterio
+from rasterio.windows import Window
 import os
 
 import sahi
 
-def read_and_slice_tiff(tiff_path, output_dir, tile_size=512, overlap_ration=0.0):
+def read_and_slice_tiff(image_path, output_dir, tile_size=512, overlap_ration=0.0):
     """
     Read a large TIFF / GeoTIFF file and divide it into slices with overlap 
     and save them as separate files
@@ -15,11 +16,37 @@ def read_and_slice_tiff(tiff_path, output_dir, tile_size=512, overlap_ration=0.0
     """
     os.makedirs(output_dir, exist_ok=True)
 
-    with rasterio.open(tiff_path) as src:
+    with rasterio.open(image_path) as src:
         width, height = src.width, src.height
         # Calculate the tile step size (width and height) for overlap
-        step_size = int(tile_size * (1 - overlap_ration))
+        tile_overlap = int(tile_size * overlap_ration)
+        step_size = tile_size - tile_overlap
 
         tile = []
-        for y in range(0, height, step_size):
-            pass
+        for y in range(0, height, step_size): # i: y
+            for x in range(0, width, step_size): # j: x
+                window = Window(x, y, tile_size, tile_size)
+                transform = src.window_transform(window)
+
+                # ensure the tile does not exceed the image bounds
+                if y + tile_size > height or x + tile_size > width:
+                    window = Window(
+                        x, y, min(tile_size, width - x), min(tile_size, height - y)
+                    )
+                
+                tile_path = os.path.join(output_dir, f"tile_{x}_{y}.tif")
+                with rasterio.open(
+                    tile.path, 
+                    "w",
+                    driver="GTiff",
+                    height=window.height,
+                    width=window.width,
+                    count=src.count,
+                    dtype=src.dtypes[0],
+                    crs=src.crs,
+                    transform=transform,
+                ) as dst:
+                    dst.write(src.read(window=window))  
+    return tile_overlap
+
+
---
+++ test/test_slice_large_image.py
@@ -0,0 +1,6 @@
+import unittest
+from preprocess.read_tiff import read_and_slice_tiff # importing the function for testing
+
+class TestSliceLargeImage(unittest.TestCase):
+    
+    pass
