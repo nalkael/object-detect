c994154ecc7b23b3a090293288c36dcf8f8176f9
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2025-02-07 10:06:04 +0100

    add some files

---
+++ mainprocess/benchmark/yolo_v5/debug.py
@@ -0,0 +1,12 @@
+import cv2
+from ultralytics import YOLO
+
+# fine-tune model on small custom dataset
+## with different parameters
+
+class YOLOv5Model:
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
