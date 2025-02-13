3491a0ebba96e619ce8f84d31c321129f7242dde
Huaixin Luo <huaixin.luo@regiodata-gmbh.de>
2025-02-13 16:42:20 +0100

    add yolo config

--- mainprocess/benchmark/yolo_v8/debug.py
+++ mainprocess/benchmark/yolo_v8/debug.py
@@ -5,16 +5,25 @@ import yaml
 import ultralytics
 from ultralytics import YOLO
 
-# load a model
-model = YOLO("yolov8x.pt")
 
 class YOLOv8Trainer:
     def __init__(self, config_path):
-        
-        self.model = YOLO(self.config["model_path"])
+        # load pre-trained model
+        # load configuration from YAML file
+        self.config = self.load_config(config_path)
+
+        # model = YOLO("yolov8x.pt")
+        self.model = YOLO(self.config["model"])
 
         # extract hyperparameters from config file
         self.data = self.config['data']
+        self.epochs = self.config['epochs']
+        self.batch_size = self.config['batch']
+        self.image_size = self.config['imgsz']
+        self.lr0 = self.config['lr0']
+        self.lrf = self.config['lrf']        
+        self.momentum = self.config['momentum']
+        
 
     
     def load_config(self, config_path):
